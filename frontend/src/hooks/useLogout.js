ad49c9ab7e95d3ce224e97cec278325d77d7bf01
Hirwa Tresor <waveb6133@gmail.com>
2024-12-30 21:01:16 +0200

    added some functionalities

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,18 +1,19 @@
-import { useAuthContext } from './useAuthContext'
-import { useWorkoutsContext } from '../context/WorkoutsContext'
+import { useAuthContext } from './useAuthContext';
+import { WorkoutsContext } from '../context/WorkoutsContext';
+import { useContext } from 'react';
 
 export const useLogout = () => {
-  const { dispatch } = useAuthContext()
-  const { dispatch: dispatchWorkouts } = useWorkoutsContext()
+  const { dispatch } = useAuthContext();
+  const { dispatch: dispatchWorkouts } = useContext(WorkoutsContext); // Use `useContext`
 
   const logout = () => {
     // remove user from storage
-    localStorage.removeItem('user')
+    localStorage.removeItem('user');
 
     // dispatch logout action
-    dispatch({ type: 'LOGOUT' })
-    dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null })
-  }
+    dispatch({ type: 'LOGOUT' });
+    dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null });
+  };
 
-  return { logout }
-}
+  return { logout };
+};
